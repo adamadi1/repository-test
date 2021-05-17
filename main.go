1a84ab3b9ebd58c71e48216a1c8de4d2b3974494
adamadi1 <83632331+adamadi1@users.noreply.github.com>
2021-05-17 12:13:16 +0200

    próba posortowania #3

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"math/rand"
 	"time"
-	
+	"fmt"
 )
 
 const ileLiczb = 500000
@@ -12,7 +12,6 @@ func main() {
 	liczby := generujLiczby(ileLiczb)
 	sortujRosnaco(liczby)
 }
-
 func generujLiczby(ilosc uint) []int {
 	wynik := make([]int, 0)
 	gen := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -25,27 +24,19 @@ func generujLiczby(ilosc uint) []int {
 }
 
 func sortujRosnaco(liczby []int) []int {
-	posortowane := make([]int, 0)
-	for j := 0; j < len(liczby); j++ {
-		aktualnaLiczbaJ := liczby[j]
+	for i:= 0; i < len(liczby)-i-1; i++ {
+		for j:= 0; j < len(liczby)-i-1; j++{
+			if (liczby[j] > liczby[j+1]) {
+				liczby[j], liczby[j+1] = liczby[j+1], liczby[j]
+			}
+		}
 	}
-	for k := 0; k < j - 1; k++ {
-		aktualnaLiczbaK := liczby[k]
-	}
-	for l :=0; l < k < j - 1; l++{
-
-	}
-	{
-	if (array[l] > array[l+1])
-	{
-		swap       = array[l]
-		array[l]   = array[l+1]
-		array[l+1] = swap;
-	}
+	return liczby
 }
+func posortowane() {
+	liczby:= []int{ileLiczb};
+	fmt.Print(sortujRosnaco(liczby))
+
+
 }
 
-
- 	// @todo zadania dla Adama
-	// na potrzeby testu zwracam liczby, ale trzeba zwracac posortowane
-	return posortowane
