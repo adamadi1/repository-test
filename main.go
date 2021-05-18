4c2cccaa60361410b508a1e041b91a4f3ccd337b
adamadi1 <83632331+adamadi1@users.noreply.github.com>
2021-05-18 13:46:43 +0200

    próba posortowania #5

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 	"time"
 	"fmt"
+	"sort"
 )
 
 const ileLiczb = 1000
@@ -12,6 +13,7 @@ func main() {
 	liczby := generujLiczby(ileLiczb)
 	sortujRosnaco(liczby)
 }
+
 func generujLiczby(ilosc uint) []int {
 	wynik := make([]int, 0)
 	gen := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -29,12 +31,38 @@ func sortujRosnaco(liczby []int) []int {
 			if (liczby[j] > liczby[j+1]) {
 				liczby[j], liczby[j+1] = liczby[j+1], liczby[j]
 			}
-		}
+			}
 		}
 	return liczby
 }
+func Sort(data Interface) {
+        for i := 1; i < data.Len(); i++ {
+            for j := i; j > 0 && data.Less(j, j-1); j-- {
+                data.Swap(j, j-1)
+            }
+        }
+    }
+	type Interface interface {
+        Len() int
+        Less(i, j int) bool
+        Swap(i, j int)
+    }
 func posortowane() {
 	liczby:= []int{ileLiczb};
 	fmt.Print(sortujRosnaco(liczby))
 }
+func ints() {
+	data := []int{10000}
+	a := sort.IntSlice(data)
+	sort.Sort(a)
+	if !sort.IsSorted(a) {
+		panic("nie działa")
+	}
 
+
+
+
+	//sort.Slice(liczby[], func(i, j int) bool {
+
+//}
+}
